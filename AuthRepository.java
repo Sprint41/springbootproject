2974ce1fa22862d46478b25415d975d5c4f5e19e
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2024-10-10 22:01:31 +0530

    Delete AuthRepository.java

--- AuthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ToDoApp.Authentication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
-
